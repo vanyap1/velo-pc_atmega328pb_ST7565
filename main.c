c4df0e142743d3481aa8d38476ba3c0c0dcedb1d
Ivan Prinst <princ.va@gmail.com>
2023-07-16 21:14:30 +0300

    New project created

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * velo-pc_atmega328pb_ST7565.c
+ *
+ * Created: 16.07.2023 21:13:26
+ * Author : princ
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
